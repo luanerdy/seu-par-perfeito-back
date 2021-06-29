7d0f698a6fc311b5fa7521a3a39e854d8dede2b0
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-29 13:07:49 -0300

    feature get products

--- src/app.js
+++ src/app.js
@@ -7,10 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//sign-up
-
-//sign-in
-
 app.get("/products", async (req, res) => {
     try {
         const request = await connection.query('SELECT * FROM products');
@@ -21,17 +17,6 @@ app.get("/products", async (req, res) => {
         console.log(err);
         res.sendStatus(500);
     }
-});
-
-app.post("/cart", async (req, res) => {
-    //check if this user already has this product
-        //if there is, UPDATES 1 in quatity
-        //if there isn't INSERT
-});
-
-app.get("/cart", async (req, res) => {
-    //check all product_cart for that user
-    //return list of products on that cart
 });
 
 export default app;
