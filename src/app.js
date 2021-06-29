0ebc7e92c06f2157ebf37e7ddef09e3edbc7cf79
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-29 13:03:40 -0300

    reset: initial files

--- src/app.js
+++ src/app.js
@@ -7,31 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//sign-up
 
-//sign-in
-
-app.get("/products", async (req, res) => {
-    try {
-        const request = await connection.query('SELECT * FROM products');
-        const products = request.rows;
-        if (request.rows.length === 0) return res.sendStatus(404);
-        res.send(products);
-    } catch(err) {
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-
-app.post("/cart", async (req, res) => {
-    //check if this user already has this product
-        //if there is, UPDATES 1 in quatity
-        //if there isn't INSERT
-});
-
-app.get("/cart", async (req, res) => {
-    //check all product_cart for that user
-    //return list of products on that cart
-});
 
 export default app;
