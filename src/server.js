dedd0118cfebd13956908816708c8afe63cd0048
luanerdy <luan.vini1@gmail.com>
2021-06-30 16:57:52 -0300

    config 2

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import app from './app.js';
 
-app.listen(4000, () => {
-    console.log("server started on port 4000");
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
 });
