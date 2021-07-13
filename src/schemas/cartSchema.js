616a26d25ed6d9d187a8689a0cbe4ae31c575983
ronaldjunior2 <ronaldjunior2@hotmail.com>
2021-07-13 15:20:02 -0300

    preparando review

--- src/schemas/cartSchema.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const cartSchema = Joi.object({
-    userId: Joi.number().required(),
-    productId: Joi.number().required(),
-    quantity: Joi.number().min(1).required(),
-});
-
-export default cartSchema;
